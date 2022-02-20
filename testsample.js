9bed1ebeabf542842a487ab001b9b9c405900ce4
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-02-20 10:05:33 -0500

    update page-template

---
+++ testsample.js
@@ -0,0 +1,45 @@
+module.exports = {
+    name: 'Abdi Hassan',
+    github: 'abdisalamhassan',
+    confirmAbout: true,
+    about:
+      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus. Proin pretium, velit eget ultricies pulvinar, sem ex eleifend elit, nec pretium ante magna ac nibh. Nulla pulvinar turpis non magna porttitor porttitor. Morbi pellentesque sed nibh eu gravida. Fusce rhoncus, turpis non ullamcorper tristique, enim nulla vehicula ligula, nec gravida augue elit nec mauris. Donec imperdiet justo et auctor vehicula. Ut eget dapibus metus. Nulla facilisi. Integer in porttitor eros.',
+    projects: [
+      {
+        name: 'Run Buddy',
+        description:
+          'A site dedicated to helping people connect with personal trainers. Also my first project',
+        languages: ['HTML', 'CSS'],
+        link: 'https://github.com/abdisalamhassan/Run-Buddy',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'Pick-A-Password',
+        description:
+          'An app designed to create passwords at random',
+        languages: ['JavaScript', 'HTML', 'CSS'],
+        link: 'https://github.com/abdisalamhassan/Pick-A-Password-',
+        feature: true,
+        confirmAddProject: true
+      },
+      {
+        name: 'How Well Do You Know Javascript?',
+        description:
+          'A quioz to help users bolster their knowledge of Javascript',
+        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+        link: 'https://github.com/abdisalamhassan/How-Well-Do-You-Know-Javascript-',
+        feature: false,
+        confirmAddProject: true
+      },
+      {
+        name: 'All In A Day.s Work',
+        description:
+          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+        languages: ['JavaScript','CSS','Moment.js','jQuery'],
+        link: 'https://github.com/abdisalamhassan/All-In-A-Day-s-Work',
+        feature: false,
+        confirmAddProject: false
+      }
+    ]
+  };
